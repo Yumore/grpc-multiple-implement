06e438ab2d4b2f7598ce316fb50921997d25e945
Nathaniel <nathanwriting@126.com>
2020-12-25 14:18:00 +0800

    增加node实现

---
+++ nodebackend/README.md
@@ -0,0 +1,17 @@
+# gRPC Chat Example with Node JS
+
+This project creates a simple command line chat using gRPC and NodeJS to show a stream and bidirectional communication.
+
+Check the full article in: https://techblog.fexcofts.com/2018/07/20/grpc-nodejs-chat-example/
+
+- Install Modules:
+
+    ``$ npm install``
+
+- Start Server:
+
+    ``$ node server``
+
+- Start client:
+
+    ``$ node client``
---
+++ nodebackend/server.js
@@ -0,0 +1,45 @@
+let grpc = require("grpc");
+var protoLoader = require("@grpc/proto-loader");
+
+const server = new grpc.Server();
+const SERVER_ADDRESS = "0.0.0.0:5001";
+
+//Load protobuf
+let proto = grpc.loadPackageDefinition(
+  protoLoader.loadSync("protos/chat.proto", {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  })
+);
+
+let users = [];
+
+//Receive message from client joining
+function join(call, callback) {
+  users.push(call);
+  notifyChat({ user: "Server", text: "new user joined ..." });
+}
+
+//Receive message from client
+function send(call, callback) {
+  notifyChat(call.request);
+}
+
+//Send message to all connected clients
+function notifyChat(message) {
+  users.forEach(user => {
+    user.write(message);
+  });
+}
+
+//Define server with the methods and start it
+server.addService(proto.example.Chat.service, { join: join, send: send });
+
+server.bind(SERVER_ADDRESS, grpc.ServerCredentials.createInsecure());
+
+server.start();
+
+console.log("Server started");
